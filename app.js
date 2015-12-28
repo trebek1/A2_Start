8f3edf636e71a91d30583e36a43d501f69d6ad17
trebek1 <trebek08@gmail.com>
2015-12-28 00:08:48 -0800

    Built Reddit in Angular 2

--- app.js
+++ app.js
@@ -8,20 +8,65 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require("angular2/angular2");
-var HelloWorld = (function () {
-    function HelloWorld() {
-        this.names = ['Ari', 'Alex', 'Phil'];
+var Article = (function () {
+    function Article(title, link) {
+        this.title = title;
+        this.link = link;
+        this.votes = 0;
     }
-    HelloWorld = __decorate([
+    Article.prototype.voteUp = function () {
+        this.votes += 1;
+    };
+    Article.prototype.voteDown = function () {
+        this.votes -= 1;
+    };
+    return Article;
+})();
+var RedditArticle = (function () {
+    function RedditArticle() {
+    }
+    RedditArticle.prototype.voteUp = function () {
+        this.article.voteUp();
+        return false;
+    };
+    RedditArticle.prototype.voteDown = function () {
+        this.article.voteDown();
+        return false;
+    };
+    RedditArticle = __decorate([
         angular2_1.Component({
-            selector: 'hello-world'
+            selector: 'reddit-article',
+            properties: ['article']
         }),
         angular2_1.View({
-            directives: [angular2_1.NgFor],
-            template: "\n\t<ul>\n\t\t<li *ng-for=\"#name of names\"> Hello {{name}} </li>\n\t</ul>\n\n\t"
+            template: "\n\t<article> \n\t\t<div class=\"votes\">{{article.votes}} </div>\n\t\t<div class=\"main\">\n\t\t\t<h2>\n\t\t\t\t<a href=\"{{link}}\"> {{article.title}} </a>\n\t\t\t</h2>\n\t\t\t<ul>\n\t\t\t\t<li><a href (click)=\"voteUp()\">upvote</a></li>\n\t\t\t\t<li><a href (click)=\"voteDown()\">downvote</a></li>\n\t\t\t</ul>\n\t\t</div>\n\t</article>\n\t"
         }), 
         __metadata('design:paramtypes', [])
-    ], HelloWorld);
-    return HelloWorld;
+    ], RedditArticle);
+    return RedditArticle;
 })();
-angular2_1.bootstrap(HelloWorld);
+var RedditApp = (function () {
+    function RedditApp() {
+        this.articles = [
+            new Article('Angular 2', 'http://angular.io'),
+            new Article('Fullstack', 'http://fullstack.io'),
+        ];
+    }
+    RedditApp.prototype.addArticle = function (title, link) {
+        this.articles.push(new Article(title.value, link.value));
+        title.value = "";
+        link.value = "";
+    };
+    RedditApp = __decorate([
+        angular2_1.Component({
+            selector: 'reddit'
+        }),
+        angular2_1.View({
+            directives: [RedditArticle, angular2_1.NgFor],
+            template: "\n\t<section class = \"new-link\">\n\t\t<div class = \"control-group\">\n\t\t\t<div> <label for=\"title\">Title: </label></div>\n\t\t\t<div><input name=\"title\" #newtitle></div>\n\t\t</div>\n\t\t<div class=\"control-group\">\n\t\t\t<div><label for=\"link\">Link:</label></div>\n\t\t\t<div><input name=\"link\" #newlink></div>\n\t\t</div>\n\n\t\t<button (click)=\"addArticle(newtitle,newlink)\"> Submit Link </button>\n\t</section> \n\n\t<reddit-article \n\t\t*ng-for=\"#article of articles\"\n\t\t[article]=\"article\">\n\n\t</reddit-article>\n\n\t"
+        }), 
+        __metadata('design:paramtypes', [])
+    ], RedditApp);
+    return RedditApp;
+})();
+angular2_1.bootstrap(RedditApp);
--- app.ts
+++ app.ts
@@ -7,25 +7,100 @@ import {
 	bootstrap,
 }from "angular2/angular2"; 
 
+class Article{
+	title: string; 
+	link: string; 
+	votes: number; 
+
+	constructor(title,link){
+		this.title = title; 
+		this.link = link; 
+		this.votes = 0; 
+	}
+
+	voteUp(){
+		this.votes +=1; 
+	}
+
+	voteDown(){
+		this.votes -=1; 
+	}
+}
+
 @Component({
-	selector: 'hello-world'
+	selector: 'reddit-article',
+	properties: ['article']
 })
 @View({
-	directives: [NgFor], 
 	template: `
-	<ul>
-		<li *ng-for="#name of names"> Hello {{name}} </li>
-	</ul>
+	<article> 
+		<div class="votes">{{article.votes}} </div>
+		<div class="main">
+			<h2>
+				<a href="{{link}}"> {{article.title}} </a>
+			</h2>
+			<ul>
+				<li><a href (click)="voteUp()">upvote</a></li>
+				<li><a href (click)="voteDown()">downvote</a></li>
+			</ul>
+		</div>
+	</article>
+	`
+})
+
+class RedditArticle{
+	voteUp(){
+		this.article.voteUp(); 
+		return false;
+	}
+	voteDown(){
+		this.article.voteDown() 
+		return false;
+	}
+}
+
+@Component({
+	selector: 'reddit'
+})
+@View({
+	directives: [RedditArticle, NgFor],
+	template: `
+	<section class = "new-link">
+		<div class = "control-group">
+			<div> <label for="title">Title: </label></div>
+			<div><input name="title" #newtitle></div>
+		</div>
+		<div class="control-group">
+			<div><label for="link">Link:</label></div>
+			<div><input name="link" #newlink></div>
+		</div>
+
+		<button (click)="addArticle(newtitle,newlink)"> Submit Link </button>
+	</section> 
+
+	<reddit-article 
+		*ng-for="#article of articles"
+		[article]="article">
+
+	</reddit-article>
 
 	`
 })
-class HelloWorld{
-	names: Array<string>; 
+class RedditApp{
+	articles: Array<Article>;
 
 	constructor(){
-		this.names = ['Ari','Alex', 'Phil'];
+		this.articles = [
+		new Article('Angular 2', 'http://angular.io'),
+		new Article('Fullstack', 'http://fullstack.io'),
+		]
 	}
+	addArticle(title, link){
+		this.articles.push(new Article(title.value, link.value));
+		title.value = "";
+		link.value = "";
+	}	
 	
 }
 
-bootstrap(HelloWorld);
+bootstrap(RedditApp);
